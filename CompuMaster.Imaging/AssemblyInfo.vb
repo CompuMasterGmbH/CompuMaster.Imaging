4ded5d62d1ee796cbeb0375a86acdd929edfc4a0
Jochen Wezel <jwezel@compumaster.de>
2016-08-26 15:55:40 +0200

    nuspec fixes

--- CompuMaster.Imaging/AssemblyInfo.vb
+++ CompuMaster.Imaging/AssemblyInfo.vb
@@ -2,7 +2,7 @@ Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("CompuMaster.Imaging")>
-<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyDescription("JIT resizing of images, EXIF tools and more")> 
 <Assembly: AssemblyCompany("CompuMaster GmbH")> 
 <Assembly: AssemblyProduct("CompuMaster Tools")>
 <Assembly: AssemblyCopyright("2004,2005,2006,2010,2015,2016 CompuMaster GmbH")>
@@ -10,4 +10,4 @@ Imports System.Runtime.InteropServices
 <Assembly: CLSCompliant(False)>
 
 <Assembly: Guid("0F5DE632-2336-4936-A626-E653CEB125CE")>
-<Assembly: AssemblyVersion("2016.08.26.100")>
+<Assembly: AssemblyVersion("2016.08.26.102")>
