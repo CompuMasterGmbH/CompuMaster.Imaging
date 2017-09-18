16dcf8c769d3c4a174f0db8ec2e639d516764bfb
Jochen Wezel <jwezel@compumaster.de>
2017-09-18 12:06:34 +0200

    updated version info

--- CompuMaster.Imaging/AssemblyInfo.vb
+++ CompuMaster.Imaging/AssemblyInfo.vb
@@ -5,9 +5,9 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("JIT resizing of images, EXIF tools and more")> 
 <Assembly: AssemblyCompany("CompuMaster GmbH")> 
 <Assembly: AssemblyProduct("CompuMaster Tools")>
-<Assembly: AssemblyCopyright("2004,2005,2006,2010,2015,2016 CompuMaster GmbH")>
+<Assembly: AssemblyCopyright("2004,2005,2006,2010,2015,2016,2017 CompuMaster GmbH")>
 <Assembly: AssemblyTrademark("camm")>
 <Assembly: CLSCompliant(False)>
 
 <Assembly: Guid("0F5DE632-2336-4936-A626-E653CEB125CE")>
-<Assembly: AssemblyVersion("2016.09.07.101")>
+<Assembly: AssemblyVersion("2017.09.18.100")>
