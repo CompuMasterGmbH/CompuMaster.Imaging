b537f9b874fa51ad70267bcd2a29a04f5ef87a18
Jochen Wezel <jwezel@compumaster.de>
2021-08-11 23:01:01 +0200

    files cleanup + git-ignores

--- nuget-build-and-push.cmd
+++
@@ -1,3 +0,0 @@
-::REM -UpdateNuGetExecutable not required since it's updated by VS.NET mechanisms
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'CompuMaster.Imaging\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\CompuMaster.Imaging\CompuMaster.Imaging.VS2015.vbproj' -verbose -NoPrompt -PushPackageToNuGetGallery"
-pause
